8468f074705da7adf68bcd059cbba2b57dc86955
samarelshamey <smsmelshamey90@gmail.com>
2024-08-19 20:57:52 +0300

    task 3

---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,8 @@
+
+const $ = require( "jquery" );
+import "./header.css";
+
+$('body').append("<div id='logo'></div>");
+$('body').append("<h1>Holberton Dashboard</h1>");
+
+console.log('Init header');
