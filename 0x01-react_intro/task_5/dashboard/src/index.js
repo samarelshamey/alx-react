080d53f7aeaeeb37a1710147e625c3f4cbed4a8b
samarelshamey <smsmelshamey90@gmail.com>
2024-08-25 20:28:10 +0300

    update

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App/App';
 import Notifications from './Notifications/Notifications';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
 	<React.StrictMode>
 		<div className='root-notifications'>
 			<Notifications />
 		</div>
 		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+	</React.StrictMode>
 );
+
