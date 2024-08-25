daa9e298b9b54abfbc66fc746caaf6c8359131bc
samarelshamey <smsmelshamey90@gmail.com>
2024-08-25 20:09:20 +0300

    update

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-import './styles.css';
-import exampleImage from './example.png';
-
-const app = document.getElementById('app');
-app.innerHTML = `
-<h1>Hello, Webpack!</h1>
-<img src="${exampleImage}" alt="Example Image" />
-`;
