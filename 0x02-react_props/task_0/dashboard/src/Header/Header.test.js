565a36dd72fdcbd1bf577894a8f027754be029f8
samarelshamey <smsmelshamey90@gmail.com>
2024-08-25 22:05:40 +0300

    ..

--- 0x02-react_props/task_0/dashboard/src/Header/Header.test.js
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import Header from './Header';
 
-test('renders header component', () => {
-    render(<Header />);
-  // Add tests later
+describe('Header component', () => {
+  it('renders without crashing', () => {
+    const { container } = render(<Header />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it('should render an img tag', () => {
+    const { getByAltText } = render(<Header />);
+    expect(getByAltText('Holberton')).toBeInTheDocument();
+  });
+
+  it('should render an h1 tag', () => {
+    const { getByText } = render(<Header />);
+    expect(getByText('School dashboard')).toBeInTheDocument();
+  });
 });
