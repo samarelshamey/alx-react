40a3b70a62e16693a172c979c577c4b0d6472e3c
samarelshamey <smsmelshamey90@gmail.com>
2024-08-26 20:11:48 +0300

    task 4

--- 0x02-react_props/task_4/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -2,23 +2,32 @@ import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
-import Notifications from '../Notifications/Notifications'
+import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
 import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
+function App({ isLoggedIn }) {
 	return (
-		<>
-			<Notifications /> {/* Add Notifications component before App */}
+		<React.Fragment>
 			<div className='App'>
-				<Header /> {/* Use the Header component */}
-				<div className='App-body'>
-					<p>Login to access the full dashboard</p>
-					<Login /> {/* Use the Login component */}
+				<div className='heading-section'>
+					<Notifications />
+					<Header />
 				</div>
-				<Footer /> {/* Use the Footer component */}
+				{isLoggedIn ? <CourseList /> : <Login />}
+				<Footer />
 			</div>
-		</>
+		</React.Fragment>
 	);
 }
+
+App.defaultProps = {
+	isLoggedIn: false,
+};
+
+App.propTypes = {
+	isLoggedIn: PropTypes.bool,
+};
 
 export default App;
