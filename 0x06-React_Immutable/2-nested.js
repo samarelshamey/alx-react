021f3767d80179ad6cc81b952dfca6034cdb66b9
samarelshamey <smsmelshamey90@gmail.com>
2024-09-12 20:53:25 +0300

    .

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -2,6 +2,5 @@ import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
 const mappedObj = fromJS(object);
-
 return mappedObj.getIn(array, undefined);
 }
