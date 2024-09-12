91d615ac98f97d7bb94c3a2997c270dd8b2c02f3
samarelshamey <smsmelshamey90@gmail.com>
2024-09-12 19:56:16 +0300

    task 2

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
+import { getIn, fromJS } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-return array.reduce((acc, key) => {
-    return acc ? acc[key] : undefined;
-}, object);
+	return getIn(fromJS(object), array);
 }
